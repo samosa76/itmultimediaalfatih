4f7f9acadb127676701bc767398bcdf0f71ea69c
Sam_76 <muhammadusamah76@gmail.com>
2023-11-30 15:21:17 +0700

    create project

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Book;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+
+class BookController extends Controller
+{
+
+    public function index():View
+    {
+
+        $books = Book::latest()->paginate(5);
+
+        return view('books.index', compact('books'));
+    }
+
+    public function create(Book $book):View
+    {
+        return view('books.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $this->validate($request, [
+            'image'     => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title'     => 'required|min:5',
+            'content'   => 'required|min:10'
+        ]);
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+        $pdf = $request->file('content');
+        $pdf->storeAs('public/pdfs', $pdf->hashName());
+
+        //create post
+        Book::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $pdf->hashName()
+        ]);
+
+        //redirect to index
+        return redirect()->route('books.index')->with(['success' => 'Data Berhasil Disimpan!']);
+    }
+
+}
---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,81 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TESTI</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+</head>
+<body style="background: lightgray">
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div>
+                    <h3 class="text-center my-4">BOOK LIST</h3>
+                    <hr>
+                </div>
+                <div class="card border-0 shadow-sm rounded">
+                    <div class="card-body">
+                        <a href="{{route('books.create')}}" class="btn btn-md btn-success mb-3">ADD NEW BOOK</a>
+                        <table class="table table-bordered">
+                            <thead>
+                              <tr>
+                                <th scope="col">GAMBAR</th>
+                                <th scope="col">JUDUL</th>
+                                <th scope="col">CONTENT</th>
+                                <th scope="col">AKSI</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              @forelse ($books as $book)
+                                <tr>
+                                    <td class="text-center">
+                                        <img src="{{ asset('storage/posts/'.$book->image) }}" class="rounded" style="width: 150px">
+                                    </td>
+                                    <td>{{ $book->title }}</td>
+                                    <td>{!! $book->content !!}</td>
+                                    <td class="text-center">
+                                        <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('books.destroy', $book->id) }}" method="POST">
+                                            <a href="{{ asset('storage/pdfs/'.$book->content) }}" class="btn btn-sm btn-dark">SHOW</a>
+                                            <a href="{{ route('books.edit', $book->id) }}" class="btn btn-sm btn-primary">EDIT</a>
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                              @empty
+                                  <div class="alert alert-danger">
+                                      Data Post belum Tersedia.
+                                  </div>
+                              @endforelse
+                            </tbody>
+                          </table>  
+                          {{ $books->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+    <script>
+        //message with toastr
+        @if(session()->has('success'))
+        
+            toastr.success('{{ session('success') }}', 'BERHASIL!'); 
+
+        @elseif(session()->has('error'))
+
+            toastr.error('{{ session('error') }}', 'GAGAL!'); 
+            
+        @endif
+    </script>
+
+</body>
+</html>
